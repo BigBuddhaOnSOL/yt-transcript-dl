479669b3066da0ae22d1ef1784d565b27c15a2a6
BigBuddhaOnSOL <BigBuddhaOnSOL@proton.me>
2025-08-04 21:48:58 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    transcript_text = ""
+    error = None
+    if request.method == 'POST':
+        youtube_url = request.form.get('youtube_url', '')
+        try:
+            if "v=" in youtube_url:
+                video_id = youtube_url.split("v=")[-1].split("&")[0]
+            elif "youtu.be/" in youtube_url:
+                video_id = youtube_url.split("youtu.be/")[-1].split("?")[0]
+            else:
+                raise ValueError("Invalid YouTube URL format")
+
+            transcript = YouTubeTranscriptApi.get_transcript(video_id)
+            formatter = TextFormatter()
+            transcript_text = formatter.format_transcript(transcript)
+
+        except Exception as e:
+            error = f"Error: {e}"
+
+    return render_template('index.html', transcript=transcript_text, error=error)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
